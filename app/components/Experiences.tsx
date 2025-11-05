e4f2452e19799f70b8116afc94238541d29c588d
Asad-Jaffery <asadmjaffery@gmail.com>
2025-11-05 13:09:08 -0800

    feat: company logos

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -4,27 +4,34 @@ import { useState } from 'react';
 
 export default function Experiences() {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const squares = [0, 1, 2, 3];
+  const images = [
+    '/tesla.png',
+    '/shopify.svg',
+    '/Recognize_logo.png',
+    '/usafacts.png',
+  ];
 
   return (
     <div className='relative z-10 flex justify-center items-center min-h-screen'>
       <div className='grid grid-cols-2 gap-4'>
         <AnimatePresence>
-          {squares.map((_, index) => {
+          {images.map((src, index) => {
             const isActive = index === activeIndex;
 
             return (
               <div key={index} className='relative'>
-                {/* Keeps layout consistent when active square is fixed */}
-                {isActive && <div className='invisible w-75 h-45' />}
+                {/* Keeps layout consistent when one is expanded */}
+                {isActive && <div className='invisible w-70 h-30' />}
 
-                <motion.div
+                <motion.img
                   layout
                   layoutId={`square-${index}`}
-                  className={`bg-blue-500 rounded-2xl cursor-pointer ring-black ${
+                  src={src}
+                  alt={`Experience ${index}`}
+                  className={`object-cover rounded-2xl cursor-pointer ring-black ${
                     isActive
-                      ? 'fixed top-1/2 left-1/2 w-75 h-45 -translate-x-1/2 -translate-y-1/2 z-50'
-                      : 'w-75 h-45'
+                      ? 'fixed top-1/2 left-1/2 w-70 h-30 -translate-x-1/2 -translate-y-1/2 z-50'
+                      : 'w-70 h-30'
                   }`}
                   transition={{
                     type: 'spring',
